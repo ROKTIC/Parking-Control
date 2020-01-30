d8f1069c071e53b0e053b623c54b226aae54de90
YUNINROK <inroktic@naver.com>
2020-01-30 16:07:31 +0900

    아코디언 + HOVER효과

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+
 $(function(){
     $("#accordian h3").click(function(){
         $("#accordian ul ul").slideUp();
@@ -5,5 +7,5 @@ $(function(){
         {
             $(this).next().slideDown();
         }
-    })
-})
+    });
+});
